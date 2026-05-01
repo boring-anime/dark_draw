c809048750dcd1069f568380131602729b71a4a3
Shubham Kumar <scishubh2684@gmail.com>
2026-05-02 04:37:26 +0530

    Initial Commit

---
+++ Dark Draw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dark Draw
+//
+//  Created by Shubham Kumar on 02/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dark DrawUITests/Dark_DrawUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Dark_DrawUITestsLaunchTests.swift
+//  Dark DrawUITests
+//
+//  Created by Shubham Kumar on 02/05/26.
+//
+
+import XCTest
+
+final class Dark_DrawUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
