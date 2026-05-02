589f3494e92f38f9f2f61823101b1976070010b0
Shubham Kumar <scishubh2684@gmail.com>
2026-05-03 00:21:14 +0530

    Enhance drawing functionality by integrating PKCanvasView and updating storyboard layout

--- Dark Draw/ViewController.swift
+++ Dark Draw/ViewController.swift
@@ -5,15 +5,60 @@
 //  Created by Shubham Kumar on 02/05/26.
 //
 
+import PencilKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate {
+    
+    private let canvasView: PKCanvasView = {
+        let canvasView = PKCanvasView()
+        canvasView.drawingPolicy = .anyInput
+        canvasView.backgroundColor = .clear
+        return canvasView
+    }()
+    
+    let drawing = PKDrawing()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        canvasView.drawing = drawing
+        canvasView.delegate = self
+        view.backgroundColor = .systemBackground
+        view.addSubview(canvasView)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        canvasView.frame = view.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let toolPicker = PKToolPicker()
+//        toolPicker.addObserver(self, forKeyPath: "selectedTool", options: .new, context: nil)
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+    
+//    Canvas
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        print("Drawing did change")
+    }
+    
+    func canvasViewDidBeginUsingTool(_ canvasView: PKCanvasView) {
+        print("Did begin using tool")
+    }
+    
+    func canvasViewDidEndUsingTool(_ canvasView: PKCanvasView) {
+        print("Did end using tool")
+    }
+    
+    func canvasViewDidFinishRendering(_ canvasView: PKCanvasView) {
+        print("Did finish rendering")
+    }
 }
 
